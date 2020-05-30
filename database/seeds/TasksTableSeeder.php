301de82847e20705cee64c6e63c89d601a4d5465
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-05-31 02:13:33 +0900

    add seeder

---
+++ database/seeds/TasksTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+// use App\Models\Task;
+use Illuminate\Database\Seeder;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+    public function run()
+    {
+      
+      //
+      for ($i = 1; $i <= 10; $i++) {
+        DB::table('tasks')->insert([
+        'title' => 'title' . $i,
+        'content' => 'content' . $i,
+        'person_in_charge' => 'person_in_charge' . $i,
+        ]);
+      }
+    }
+}
